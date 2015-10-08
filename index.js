c151e5469e0b2189c065ecafd4b279f2d58d70aa
Max White <mushishi78@gmail.com>
2015-10-08 03:08:13 +0100

    Accidentally remove done function

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function StaticWebpackPlugin(bundlePath) {
 StaticWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
 
-  compiler.plugin('emit', function(compilation) {
+  compiler.plugin('emit', function(compilation, done) {
     var source = compilation.assets[self.bundlePath].source();
     var render = evaluate(source, self.bundlePath, undefined, true);
-    render(addToAssets(compilation.assets));
+    render(addToAssets(compilation.assets), done);
   });
 };
 
