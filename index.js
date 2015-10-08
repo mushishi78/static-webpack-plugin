b31d81eb29604bdb4109656157c19609148a46ac
Max White <mushishi78@gmail.com>
2015-10-08 02:02:09 +0100

    Use emit callback rather than after-compile

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function StaticWebpackPlugin(bundlePath) {
 StaticWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
 
-  compiler.plugin('after-compile', function(compilation, done) {
+  compiler.plugin('emit', function(compilation, done) {
     var source = compilation.assets[self.bundlePath].source();
     var render = evaluate(source, self.bundlePath, undefined, true);
     render(addToAssets(compilation.assets), done);
